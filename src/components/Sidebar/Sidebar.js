bbb9820ba5928d6e21dc6499eea799b64468282e
Kristian Ivanov <kriskopk@abv.bg>
2022-12-14 19:33:50 +0200

    Created Sidebar component.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,21 +1,86 @@
 import React from "react";
 import "./Sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AccountBoxRoundedIcon from '@mui/icons-material/AccountBoxRounded';
+import StoreRoundedIcon from '@mui/icons-material/StoreRounded';
+import LocalMallRoundedIcon from '@mui/icons-material/LocalMallRounded';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import InsertChartOutlinedOutlinedIcon from '@mui/icons-material/InsertChartOutlinedOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PeopleOutlineOutlinedIcon from '@mui/icons-material/PeopleOutlineOutlined';
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className="sidebar">
       <div className="top">
         <span className="logo">IvanovAdmin</span>
       </div>
+      <hr />
       <div className="center">
         <ul>
-          <li><span>Dashboard</span></li>
-          <li><span>Dashboard</span></li>
-          <li><span>Dashboard</span></li>
-          <li><span>Dashboard</span></li>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon"/>
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <PeopleOutlineOutlinedIcon className="icon"/>
+            <span>Users</span>
+          </li>
+          <li>
+            <StoreRoundedIcon className="icon"/>
+            <span>Products</span>
+          </li>
+          <li>
+            <LocalMallRoundedIcon className="icon"/>
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon"/>
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <InsertChartOutlinedOutlinedIcon className="icon"/>
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsOutlinedIcon className="icon"/>
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon"/>
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon"/>
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsOutlinedIcon className="icon"/>
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountBoxRoundedIcon className="icon"/>
+            <span>Profile</span>
+          </li>
+          <li>
+            <LoginOutlinedIcon className="icon"/>
+            <span>Logout</span>
+          </li>
         </ul>
       </div>
-      <div className="bottom">color options</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   );
 };
