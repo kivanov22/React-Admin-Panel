f7604903b1343a4ba86e427d1ef11edc2225f84c
Kristian Ivanov <kriskopk@abv.bg>
2022-12-18 15:52:03 +0200

    Created component Datatable and datatablesource file with sample data.

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Datatable from '../../components/Datatable/Datatable.js'
 import Navbar from '../../components/Navbar/Navbar.js'
 import Sidebar from '../../components/Sidebar/Sidebar.js'
 import "./List.scss"
@@ -10,7 +11,7 @@ const List = () => {
       <Sidebar />
       <div className="listContainer">
         <Navbar />
-        datatable
+        <Datatable />
       </div>
     </div>
   )
