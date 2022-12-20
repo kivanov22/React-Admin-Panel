c5cbd2df64b8a012a9161109dffaa3d30998477a
Kristian Ivanov <kriskopk@abv.bg>
2022-12-20 14:06:48 +0200

    Single page logic design and logic done.

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,13 +4,12 @@ import {
   AreaChart,
   Area,
   XAxis,
-  YAxis,
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
 
-const Chart = () => {
+const Chart = ({aspect,title}) => {
   const data = [
     {name:"January", Total: 1200},
     {name:"February", Total: 2100},
@@ -22,8 +21,8 @@ const Chart = () => {
 
   return (
     <div className="chart">
-      <div className="title">Last 6 Months (Revenue)</div>
-      <ResponsiveContainer width="100%" aspect={2/1}>
+      <div className="title">{title}</div>
+      <ResponsiveContainer width="100%" aspect={aspect}>
         <AreaChart
           width={730}
           height={250}
--- src/pages/single/Single.js
+++ src/pages/single/Single.js
@@ -1,10 +1,56 @@
-import React from 'react'
-import "./Single.scss"
+import React from "react";
+import Chart from "../../components/Chart/Chart.js";
+import Navbar from "../../components/Navbar/Navbar.js";
+import Sidebar from "../../components/Sidebar/Sidebar.js";
+import List from "../../components/Table/Table.js";
+import "./Single.scss";
 
 const Single = () => {
   return (
-    <div>Single</div>
-  )
-}
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img   src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+               alt=""
+                className="itemImg" />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">Janedoes@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">0898520630</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">Elton St. 234 Garden Yd. New York</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3/1} title="User Spending ( Last 6 Months )"/>
+          </div>
+        </div>
+        <div className="bottom">
+        <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Single
+export default Single;
